7114fdc54c2ac0d81ceeb5ea0c9dd8063ad12e0d
Ethan Liu <108840760+EthanL818@users.noreply.github.com>
2024-06-14 09:55:08 -0400

    implemented reactive navbar behaviour

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1238679a567ff1e8"; }
+/******/ 		__webpack_require__.h = function() { return "7995d04fcd2bd78f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../lib/context";
 import { auth } from "../lib/firebase";
 
 export default function Navbar() {
   // real time state, will update
   const { user, username } = useContext(UserContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const router = useRouter();
 
@@ -15,32 +16,40 @@ export default function Navbar() {
     router.reload();
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link href="/">
             <button className="btn-logo">
-              {" "}
               <span className="curly-braces">devCircle</span>
             </button>
           </Link>
         </li>
 
+        {/* Hamburger icon */}
+        <li className="hamburger" onClick={toggleMenu}>
+          ☰
+        </li>
+
         {/* user is signed in and has a username */}
         {username && (
           <>
-            <li className="push-left">
+            <li className="push-left desktop-only">
               <button onClick={signOut}>Sign Out</button>
             </li>
 
-            <li>
-              <Link className href="/admin">
-                <button className="btn-blue"> Write Posts</button>
+            <li className="desktop-only">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
               </Link>
             </li>
 
-            <li>
+            <li className="desktop-only">
               <Link href={`/${username}`}>
                 <img src={user?.photoURL || "avatar.jpg"} />
               </Link>
@@ -50,13 +59,34 @@ export default function Navbar() {
 
         {/* user is signed in and has a username */}
         {!username && (
-          <>
+          <li className="desktop-only">
             <Link href="/enter">
               <button className="btn-blue">Log in</button>
             </Link>
-          </>
+          </li>
         )}
       </ul>
+
+      {/* Collapsible menu */}
+      {menuOpen && (
+        <div className="dropdown-menu">
+          {username ? (
+            <>
+              <button onClick={signOut}>Sign Out</button>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL || "avatar.jpg"} />
+              </Link>
+            </>
+          ) : (
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          )}
+        </div>
+      )}
     </nav>
   );
 }
