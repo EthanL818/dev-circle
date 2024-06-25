26717ce858d574fe34ab7736aeeb5ff302231b1e
Ethan Liu <108840760+EthanL818@users.noreply.github.com>
2024-06-25 14:43:08 -0400

    attempted fix for vercel deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6d0fadcff81af58"; }
+/******/ 		__webpack_require__.h = function() { return "b445b49b13f75b06"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
